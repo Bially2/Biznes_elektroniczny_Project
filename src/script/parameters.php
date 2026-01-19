87effd441ed7bb5064dc9381e6167a5af2e2ce90
Szymon <s197949@student.pg.edu.pl>
2026-01-19 21:41:50 +0100

    Add parameters.php and update deploy scripts for multi-node support

---
+++ src/script/parameters.php
@@ -0,0 +1,23 @@
+<?php
+return array(
+    'parameters' => array(
+        'database_host' => 'admin-mysql_db',
+        'database_port' => '3306',
+        'database_name' => 'BE_192914',
+        'database_user' => 'root',
+        'database_password' => 'student',
+        'database_prefix' => 'ps_',
+        'database_engine' => 'InnoDB',
+        'mailer_transport' => 'smtp',
+        'mailer_host' => '127.0.0.1',
+        'mailer_user' => null,
+        'mailer_password' => null,
+        'secret' => 'PrestaShopIsAwesomeSecretKey123456789',
+        'ps_caching' => 'CacheMemcache',
+        'ps_cache_enable' => false,
+        'ps_creation_date' => '2024-01-01',
+        'locale' => 'pl',
+        'cookie_key' => 'PrestaShopCookieKey123456789',
+        'cookie_iv' => 'PrestaShopCookieIV',
+    ),
+);
